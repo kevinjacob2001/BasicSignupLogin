d189f4cf09c3b2cbe6ec07a2791d2857344e9c18
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-07-23 12:25:20 +0530

    added firebas signin and signup

--- src/Login.js
+++ src/Login.js
@@ -6,7 +6,7 @@ class Login extends Component{
         super(props)
         this.login=this.login.bind(this);
         this.handleChange=this.handleChange.bind(this);
-        this.signup=this.signup.bind(this);
+        this.signup=this.signup.bind(this)
         this.state={
             email:"",
             password:""
@@ -24,11 +24,16 @@ console.log(err);
 
 signup(e){
     e.preventDefault();
-    fire.auth().signInWithEmailAndPassword(this.state.email,this.state.password).then((u)=>{
-        console.log(u)
-    }).catch((err)=>{
-        console.log(err)
-    })
+    fire.auth().createUserWithEmailAndPassword(this.state.email,this.state.password).then(function(user) {
+        // User is created and signed in, do whatever is needed
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log('User did not sign up correctly');
+        console.log(errorCode);
+        console.console.log(errorMessage);
+      });
 }
 
 handleChange(e){
