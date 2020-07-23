544dfddd21583382ffcb70e1a3894f909d8bc2f5
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-07-23 15:39:49 +0530

    Updates

---
+++ precache-manifest.9c65b2df8c0325bb8201d83198bc8a2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1074123663513750327a6ba3f476a317",
+    "url": "/UserAuth/index.html"
+  },
+  {
+    "revision": "423005ccca01a6dfdbd4",
+    "url": "/UserAuth/static/css/main.499a61cc.chunk.css"
+  },
+  {
+    "revision": "2ab76d3452b78703e8e6",
+    "url": "/UserAuth/static/js/2.8f587237.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/UserAuth/static/js/2.8f587237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "423005ccca01a6dfdbd4",
+    "url": "/UserAuth/static/js/main.ec768550.chunk.js"
+  },
+  {
+    "revision": "7be883fac4073be54e84",
+    "url": "/UserAuth/static/js/runtime-main.cfc80c0b.js"
+  }
+]);
